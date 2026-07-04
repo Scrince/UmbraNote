9f2f8cbd90ea5b4ceed4bdab9fd33cf339d3d96b
Administrator <48503653+Scrince@users.noreply.github.com>
2026-07-04 17:53:31 -0400

    Create resource.h

---
+++ platform/win32/resource.h
@@ -0,0 +1,48 @@
+#define IDI_ZERONOTE          101
+#define IDR_MENU              102
+#define IDR_ACCEL             103
+
+#define IDD_FIND              201
+#define IDD_REPLACE           202
+#define IDD_GOTO              203
+#define IDD_PASSWORD          205
+
+#define IDC_EDIT              1001
+#define IDC_STATUS            1002
+
+#define IDC_FINDWHAT          2001
+#define IDC_REPLACEWHAT       2002
+#define IDC_REPLACEWITH       2003
+#define IDC_MATCHCASE         2004
+#define IDC_GOTO_LINE         2005
+#define IDC_PASSWORD          2006
+#define IDC_PASSWORD_CONFIRM  2007
+#define IDC_PASSWORD_LABEL    2008
+#define IDC_PASSWORD_CONFIRM_LABEL 2009
+#define IDC_KEYFILE_LABEL          2010
+#define IDC_KEYFILE_PATH           2011
+#define IDC_KEYFILE_BROWSE         2012
+#define IDC_PARANOID_KDF           2013
+
+#define IDM_FILE_NEW          40001
+#define IDM_FILE_OPEN         40002
+#define IDM_FILE_SAVE         40003
+#define IDM_FILE_SAVEAS       40004
+#define IDM_FILE_PRINT        40006
+#define IDM_FILE_EXIT         40007
+#define IDM_FILE_SAVEENCRYPTED 40008
+#define IDM_FILE_EXPORTPDF    40009
+
+#define IDM_EDIT_UNDO         40010
+#define IDM_EDIT_CUT          40011
+#define IDM_EDIT_COPY         40012
+#define IDM_EDIT_PASTE        40013
+#define IDM_EDIT_DELETE       40014
+#define IDM_EDIT_FIND         40015
+#define IDM_EDIT_FINDNEXT     40016
+#define IDM_EDIT_REPLACE      40017
+#define IDM_EDIT_GOTO          40018
+#define IDM_EDIT_SELECTALL    40019
+
+#define IDM_FORMAT_WORDWRAP   40030
+#define IDM_FORMAT_FONT       40031
