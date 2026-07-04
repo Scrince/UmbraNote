6e2bf76b5b239571d1c5d7e0f5a32369f3d0e870
Administrator <48503653+Scrince@users.noreply.github.com>
2026-07-04 17:53:34 -0400

    Create notepad.h

---
+++ platform/win32/notepad.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+#ifndef _UNICODE
+#define _UNICODE
+#endif
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+#include <commctrl.h>
+#include <commdlg.h>
+#include <string>
+
+struct AppState {
+    HINSTANCE hInstance = nullptr;
+    HWND hwndMain = nullptr;
+    HWND hwndEdit = nullptr;
+    HWND hwndStatus = nullptr;
+    HFONT hFont = nullptr;
+    HFONT hUiFont = nullptr;
+    HBRUSH hEditBrush = nullptr;
+    LOGFONTW lf{};
+    std::wstring filePath;
+    bool modified = false;
+    bool wordWrap = true;
+    std::wstring findText;
+    bool findMatchCase = false;
+    bool fileEncrypted = false;
+    bool encryptedRequiresKeyfile = false;
+    bool encryptedParanoidKdf = true;
+    HACCEL hAccel = nullptr;
+};
+
+extern AppState g_app;
+
+void UpdateTitle(HWND hwnd);
+void UpdateMenuState(HWND hwnd);
+void UpdateStatusBar();
+void ResizeControls(HWND hwnd);
+void ApplyFont();
+void ApplyWordWrap(HWND hwnd);
+void SetModified(bool modified);
+void SyncModifiedFromEdit();
+
+bool ConfirmSaveIfNeeded(HWND hwnd);
+void DoFileNew(HWND hwnd);
+void DoFileOpen(HWND hwnd);
+bool DoFileSave(HWND hwnd);
+bool DoFileSaveAs(HWND hwnd);
+bool DoFileSaveEncryptedAs(HWND hwnd);
+void DoFileExportPdf(HWND hwnd);
+void DoFilePrint(HWND hwnd);
+void ClearEncryptionSession();
+void DoFileExit(HWND hwnd);
+
+void DoEditUndo();
+void DoEditCut();
+void DoEditCopy();
+void DoEditPaste();
+void DoEditDelete();
+void DoEditFind(HWND hwnd);
+void DoEditFindNext(HWND hwnd);
+void DoEditReplace(HWND hwnd);
+void DoEditGoTo(HWND hwnd);
+void DoEditSelectAll();
+
+void DoFormatWordWrap(HWND hwnd);
+void DoFormatFont(HWND hwnd);
+
+INT_PTR CALLBACK FindDlgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+INT_PTR CALLBACK ReplaceDlgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+INT_PTR CALLBACK GoToDlgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+INT_PTR CALLBACK PasswordDlgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
