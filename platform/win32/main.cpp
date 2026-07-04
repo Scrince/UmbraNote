651b40a09fd8cf4f0e56bb07f319305a48f4ad0f
Administrator <48503653+Scrince@users.noreply.github.com>
2026-07-04 17:53:42 -0400

    Create main.cpp

---
+++ platform/win32/main.cpp
@@ -0,0 +1,58 @@
+#include "notepad.h"
+#include "resource.h"
+
+#include <shellscalingapi.h>
+
+#pragma comment(lib, "shcore.lib")
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow) {
+    SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE);
+
+    g_app.hInstance = hInstance;
+    g_app.hAccel = LoadAcceleratorsW(hInstance, MAKEINTRESOURCEW(IDR_ACCEL));
+
+    const wchar_t* className = L"UmbraNoteClass";
+
+    WNDCLASSEXW wcex{};
+    wcex.cbSize = sizeof(wcex);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIconW(nullptr, IDI_APPLICATION);
+    wcex.hCursor = LoadCursorW(nullptr, IDC_IBEAM);
+    wcex.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDR_MENU);
+    wcex.lpszClassName = className;
+    wcex.hIconSm = LoadIconW(nullptr, IDI_APPLICATION);
+
+    if (!RegisterClassExW(&wcex)) {
+        MessageBoxW(nullptr, L"Failed to register window class.", L"UmbraNote",
+                    MB_OK | MB_ICONERROR);
+        return 1;
+    }
+
+    HWND hwnd = CreateWindowExW(
+        0, className, L"Untitled - UmbraNote",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 640, 480,
+        nullptr, nullptr, hInstance, nullptr);
+
+    if (!hwnd) {
+        MessageBoxW(nullptr, L"Failed to create main window.", L"UmbraNote",
+                    MB_OK | MB_ICONERROR);
+        return 1;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    MSG msg{};
+    while (GetMessageW(&msg, nullptr, 0, 0)) {
+        if (!TranslateAcceleratorW(hwnd, g_app.hAccel, &msg)) {
+            TranslateMessage(&msg);
+            DispatchMessageW(&msg);
+        }
+    }
+
+    return static_cast<int>(msg.wParam);
+}
